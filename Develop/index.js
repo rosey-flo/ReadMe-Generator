78d91971c959a02041efddf188f72db64cd03ace
rosey-flo <emilyflo2012@yahoo.com>
2024-07-16 19:01:16 -0400

    functional code completed

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,79 @@
-// TODO: Include packages needed for this application
-
 // TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please type in the title of your project'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please type in a description explaining the what, why, and how of your application.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter a a step-by-step description required for other others to install your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter instructions on the usage including any assisting screenshots.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: ['MIT license', 'Apache 2.0', 'GNU General Public v3.0', 'Boost', 'BSD 2-Clause', 'Creative Commons', 'Eclipse Public', 'IBM', 'Mozilla Public', 'Perl']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter any contributers towards the creation of your application as well as their Github or webpages if applicable'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter any tests that would be useful in demonstrating your application along with instructions'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please enter your Github Username so users can reach out with any questions'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeREADME(readmeData, filename = './README.md') {
+    // Generate the README content using generateMarkdown function
+    const generatedREADME = generateMarkdown(readmeData);
+
+    // Write the README file
+    fs.writeFile(filename, generatedREADME, err => {
+        if (err) {
+            console.error(`Error writing file ${filename}:`, err);
+        } else {
+            console.log(`README file ${filename} has been successfully generated!`);
+        }
+    });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // Prompt user with questions using inquirer
+    inquirer.prompt(questions)
+        .then(answers => {
+            // Pass answers (readmeData) to writeREADME function
+            writeREADME(answers);
+        })
+        .catch(error => {
+            console.error('Error occurred during prompt:', error);
+        });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,83 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // if string is empty, this will output an empty string
+  if (license) {
+    return `![License](https://img.shields.io/badge/License-${license}-lightblue.svg)`
+  } else {
+    return '';
+  };
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  // const licenseLinks = [
+  //   '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)', '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)', '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)', '![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)', '![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)', '![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)', '![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)', '![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)', '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)', '![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)'
+  //];
+  if (license) {
+    return '[License](#license)';
+  } else {
+    return ''; // Return empty string if no valid license provided
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+//   const licenseInfo = {
+//   badge: licenseLinks[license],
+//   liscenseName: licenseBadges[license]
+// };
+
+  if (license) {
+    return `## License
+
+    This project is licensed under the ${license} license.`;
+  } else {
+    return '';
+}
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown({title, description, installation, usage, license, contributing, tests, username}) {
+  const markdown = 
+    `# ${title}
 
-`;
-}
+    ${renderLicenseBadge(license)}
+
+    ## Description
+    ${description}
+
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - ${renderLicenseLink(license)}
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ##Installation
+    ${installation}
+
+    ## Usage
+    ${usage}
+
+    ## License
+    This application is covered by the ***${license}***.
+    
+    ## Contributing
+    ${contributing}
+
+    ## Tests
+    ${tests}
+
+    ## Questions
+    Should you have any specific questions regarding my application or would like to compare with my other deployed applcations, contact and general information can be found on my Github homepage found here: GitHub: [@${username}](https://github.com/${username})
+    `;
+  
+  return markdown;
+};
 
 module.exports = generateMarkdown;
